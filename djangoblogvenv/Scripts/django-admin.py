41e791a4c622b1edd5640e49e0822c01aa90a17f
Gokul <gokulsrinivask2020@gmail.com>
2022-10-05 18:28:07 +0530

    My django blog app , first commit

---
+++ djangoblogvenv/Scripts/django-admin.py
@@ -0,0 +1,10 @@
+#!C:\Users\GOKUL SRINIVAS K\Python-django-blog\djangoblog\djangoblogvenv\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+
+from django.core import management
+
+
+
+if __name__ == "__main__":
+    
+    management.execute_from_command_line()
